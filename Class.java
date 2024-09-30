45d7d55106bc0ea87babc33d69986e5ec69e6078
DanikDaraboz <denchikgg2006@gmail.com>
2024-09-30 23:21:03 +0500

    Добавлен класс magazine в Class.java

--- Class.java
+++ Class.java
@@ -1,20 +1,49 @@
+// Class.java
 public class Class {
-
-    public double convert(double celsius) {
-        return (celsius * 9/5) + 32;
-    }
-
-    public void displayConversion(double celsius) {
-        double fahrenheit = convert(celsius);
-        System.out.printf("%.2f Celsius is equal to %.2f Fahrenheit.%n", celsius, fahrenheit);
-    }
-
     public static void main(String[] args) {
-        Class converter = new Class();
-        double celsius = 25;
-        converter.displayConversion(celsius);
+        Magazine magazine = new Magazine("Tech Today", "Tech Publishers", "September 2024", 5.99);
+
+        magazine.displayInfo();
     }
 }
+
+class Magazine {
+    private String title;
+    private String publisher;
+    private String releaseDate;
+    private double price;
+
+    public Magazine(String title, String publisher, String releaseDate, double price) {
+        this.title = title;
+        this.publisher = publisher;
+        this.releaseDate = releaseDate;
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void displayInfo() {
+        System.out.println("Title: " + title);
+        System.out.println("Publisher: " + publisher);
+        System.out.println("Release Date: " + releaseDate);
+        System.out.println("Price: $" + price);
+    }
+}
+
 
 
 
